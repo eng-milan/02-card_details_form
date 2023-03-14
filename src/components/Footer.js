bd82c501bc48cb79e1c7393a5aac7a5be7199a0a
eng-milan <tohid_d.milan@yahoo.com>
2023-03-13 20:23:12 -0600

    fix image cover and mobile view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 
 function Footer({ className }) {
     return <div className={`attribution text-center fixed bottom-0 lg:left-1/3 ${className}`}>
-        <h1>
+        <h1 className="text-[14px]">
             Challenge by <a className="text-blue-800 hover:text-blue-500" href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
             Coded by <a className="text-blue-800 hover:text-blue-500" href="mailto:tohid_d.milan@yahoo.com">Tohid Dalaei Milan</a>.
         </h1>
